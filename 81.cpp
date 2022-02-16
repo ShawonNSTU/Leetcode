ec25574a66088f93f372d8d3fa712fa129fbee1a
ShawonNSTU <shuvroshawon9@gmail.com>
2022-02-16 21:47:17 +0600

    Add files via upload

---
+++ 81.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(nums[mid] == target) return true;
+            
+            while(l < mid && nums[l] == nums[mid]) ++l;
+            while(r > mid && nums[r] == nums[mid]) --r;
+            
+            if(nums[l] <= nums[mid]) {
+                if(nums[mid] < target || nums[l] > target) l = mid + 1;
+                else r = mid - 1;
+            }
+            else {
+                if(nums[mid] > target || nums[r] < target) r = mid - 1;
+                else l = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+};
