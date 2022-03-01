7eed03fb7a164b412ea3ee04a44a3e98c76ae039
ShawonNSTU <shuvroshawon9@gmail.com>
2022-03-01 20:09:11 +0600

    Update 338.cpp

--- 338.cpp
+++ 338.cpp
@@ -14,3 +14,16 @@ public:
         return res;
     }
 };
+
+// Java DP Approach
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        int top = 1;
+        for(int i = 1; i <= n; i++) {
+            if(top * 2 == i) top = i;
+            ans[i] = 1 + ans[i-top];
+        }
+        return ans;
+    }
+}
