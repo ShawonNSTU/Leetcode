f79bb02b64131f2a1ccc08183b036bd2de6e7f7e
ShawonNSTU <shuvroshawon9@gmail.com>
2022-03-05 18:21:29 +0600

    Update 740.cpp

--- 740.cpp
+++ 740.cpp
@@ -1,3 +1,4 @@
+// Tabulation/Bottom-Up DP
 class Solution {
 public:
     const int MAX = 10001;
@@ -22,5 +23,24 @@ public:
             prev2 = max(c1, c2);
         }
         return max(prev1, prev2);
+    }
+};
+
+// Memorization/Top-Down DP
+class Solution {
+public:
+    const int MAX = 10001;
+    int getMaxPoint(vector<int>& cnt, vector<int>& dp, int idx) {
+        if(idx >= MAX) return 0;
+        if(dp[idx] == -1) {
+            dp[idx] = max(idx * cnt[idx] + getMaxPoint(cnt, dp, idx + 2), getMaxPoint(cnt, dp, idx + 1));
+        }
+        return dp[idx];
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        vector<int> cnt(MAX, 0), dp(MAX);
+        for(int i = 0; i < MAX; ++i) dp[i] = -1;
+        for(int i = 0; i < nums.size(); ++i) cnt[nums[i]]++;
+        return getMaxPoint(cnt, dp, 1);
     }
 };
