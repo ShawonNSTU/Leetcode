ca8bf5e30ec781f4e4b32aa262ac33860e516c02
ShawonNSTU <shuvroshawon9@gmail.com>
2022-03-14 23:08:42 +0600

    Add files via upload

---
+++ 861.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int binaryToDecimal(string num) {
+        int decValue = 0, base = 1, len = num.length();
+        for(int i = len - 1; i >= 0; --i) {
+            if(num[i] == '1') decValue += base;
+            base *= 2;
+        }
+        return decValue;
+    }
+    int matrixScore(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        for(int i = 0; i < m; ++i) {
+            if(!grid[i][0]) {
+                for(int j = 0; j < n; ++j) grid[i][j] ^= 1;
+            }
+        }
+        for(int i = 0; i < n; ++i) {
+            int one = 0;
+            for(int j = 0; j < m; ++j) {
+                if(grid[j][i]) ++one;
+            }
+            if(m - one >= one) {
+                for(int j = 0; j < m; ++j) grid[j][i] ^= 1;
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < m; ++i) {
+            string num = "";
+            for(int j = 0; j < n; ++j) num += grid[i][j] + 48;
+            sum += binaryToDecimal(num);
+        }
+        return sum;
+    }
+};
