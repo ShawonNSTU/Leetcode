4e50ad0a122f6b85e84cf07a46b6570b909e7f7a
ShawonNSTU <shuvroshawon9@gmail.com>
2022-03-22 01:14:15 +0600

    Update 763.cpp

--- 763.cpp
+++ 763.cpp
@@ -27,3 +27,34 @@ public:
         return res;
     }
 };
+
+// C# Language
+public class Solution {
+    public static int Compare(KeyValuePair<int, int> a, KeyValuePair<int, int> b) {
+        return a.Key.CompareTo(b.Key);
+    }
+    public IList<int> PartitionLabels(string s) {
+        int[] lPos = new int[26], rPos = new int[26];
+        for(int i = 0; i < s.Length; ++i) rPos[s[i]-'a'] = i + 1;
+        for(int i = s.Length - 1; i >= 0; --i) lPos[s[i]-'a'] = i + 1;
+        List<KeyValuePair<int, int>> posList = new List<KeyValuePair<int, int>>();
+        for(int i = 0; i < 26; ++i) {
+            if(lPos[i] > 0) {
+                posList.Add(new KeyValuePair<int, int>(lPos[i], rPos[i]));
+            }
+        }
+        posList.Sort(Compare);
+        List<int> res = new List<int>();
+        int lMin = posList[0].Key, rMax = posList[0].Value;
+        for(int i = 1; i < posList.Count; ++i) {
+            if(posList[i].Key > rMax) {
+                res.Add(rMax - lMin + 1);
+                lMin = posList[i].Key;
+                rMax = posList[i].Value;
+            }
+            else rMax = Math.Max(rMax, posList[i].Value);
+        }
+        res.Add(rMax - lMin + 1);
+        return res;
+    }
+}
